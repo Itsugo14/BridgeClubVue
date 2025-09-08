1d445091e5752893dc56b60b064c8c452f5fe8c0
Mads Aagaard Larsen <mal012@edu.zealand.dk>
2025-09-08 10:20:37 +0200

    Outcomment app.js

--- app.js
+++ app.js
@@ -1,43 +1,30 @@
+// const baseUrl = "Indsæt baseURL her";
+// Vue.createApp({
+//   data() {
+//     return {
+//       FirstName: "",
+//       SurName: "",
+//       Adress1: "",
+//       Adress2: "",
+//       Country: "",
+//       PostCode: [],
+//       Phone: [],
+//       Email: "",
+//       Password: "",
+//       DateOfBirth: "",
+//       NewsLetter: false
+//     }
+//   },
+//   methods: {
+//     addUser() {
+//       this.addUser(baseUrl)
+//     },
+//     Login() {
+//       this.Login(baseUrl)
+//     },
+//     sendRecoveryCode() {
+//       this.sendRecoveryCode(baseUrl)
+//     }
 
-
-// use https (http secure).
-// http (non-secure) will make the app complain about mixed content when running the app from Azure
-const baseUrl = "https://bridgeclubapi-bvhue4gpaahmdjbs.northeurope-01.azurewebsites.net/api/MembersDb";
-
-Vue.createApp({
-  data() {
-    return {
-      FirstName: "",
-      SurName: "",
-      Adress1: "",
-      Adress2: "",
-      Country: "",
-      PostCode: [],
-      Phone: [],
-      Email: "",
-      Password: "",
-      DateOfBirth: ""
-    }
-  },
-  methods: { // Get all members
-    getAllMembers() {
-      this.getAllMembers(baseUrl)
-    },
-    getById(id) { // Get member by ID
-      this.getById(`${baseUrl}/${id}`)
-    },
-    addMember() { // Add new member
-      this.addMember(baseUrl)
-    },
-    Login() {
-      this.Login(baseUrl)
-    },
-    sendRecoveryCode() {
-      this.sendRecoveryCode(baseUrl)
-    },
-    CheckLogin() {
-      this.CheckLogin(baseUrl)
-    }
-
-  }
-}).mount("#app")
+//   }
+// }).mount("#app")
